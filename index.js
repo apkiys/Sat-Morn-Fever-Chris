4a4f81d20a810e93a1a003323e5a60d3045d1de5
apkiys <20112494+apkiys@users.noreply.github.com>
2023-08-19 13:05:18 +0800

    Upload changes to repository

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Your function executed successfully!",
-        },
-        null,
-        2
-      ),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Your function executed successfully!",
+        access_local_key: process.env.access_local_key,
+        access_key: process.env.access_key
+      },
+      null,
+      2
+    ),
   };
+};
